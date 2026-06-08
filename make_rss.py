1bc916d18840d77c834fba812a8b8bdb34acf7cd
twochaemi-dotcom <twochaemi@gmail.com>
2026-06-08 13:17:32 +0900

    Update make_rss.py

--- make_rss.py
+++ make_rss.py
@@ -5,20 +5,27 @@ from rfeed import Item, Feed
 
 # 1. 수집 대상 피드 리스트 (The Verge 메인 피드 포함)
 urls = [
+    #모빌리티 & EV 전문 매체 (전체수집)
     "https://carapp-news.com/feed/",
     "https://electrek.co/feed/",
     "https://www.notateslaapp.com/rss/",
+    "https://cleantechnica.com/feed/"
+    "https://www.greencarreports.com/news/rss-feed"
+    "https://news.google.com/rss/search?q=site:greencarcongress.com&hl=en-US&gl=US&ceid=US:en"
+    #종합 자동차 및 테크 전문 매체 (카테고리/키워드 선별 수집)
     "https://www.autonews.com/arc/outboundfeeds/sitemap-news/",
     "https://techcrunch.com/category/transportation/feed/",
-    "https://news.google.com/rss/search?q=site:greencarcongress.com&hl=en-US&gl=US&ceid=US:en"
-    "https://feeds.feedburner.com/harvardbusinessreview"
-    "https://www.greencarreports.com/news/rss-feed"
-    "https://cleantechnica.com/feed/"
     "https://www.smartcitiesdive.com/feeds/news/"
+    #종합 기술 동향 매체 (키워드 선별 수집)
+    "https://www.theverge.com/rss/index.xml"
+    "https://feeds.feedburner.com/harvardbusinessreview"
+    "https://www.technologyreview.com/feed/"
 ]
 
 # 필터링할 모빌리티 관련 키워드 리스트
-mobility_keywords = ['transport', 'car', 'ev', 'vehicle', 'autonomous', 'mobility', 'robotaxi', 'fleet', 'automotive']
+mobility_keywords = ['transport', 'car', 'EV', 'AV', 'electronic', 'vehicle', 'autonomous', 'mobility', 'robotaxi', 'fleet', 'automotive', 'OTA', 'Over-the-Air', 'CCS', 'connected car', 'FoD']
+# 필터링할 기술, 서비스 관련 키워드 리스트
+technology_keywords = ['app', 'superapp', 'platform', 'membership', 'fintech', 'subscription', 'subscribe', 'payment', 'AI', 'agent', 'Artificial Intelligence', 'personalization', 'LLM', 'Large Language Model', 'model', 'assistant', 'OS', 'UX']
 
 items = []
 now = datetime.now()
@@ -45,8 +52,8 @@ for url in urls:
                     content_text = (title + " " + summary).lower()
                     
                     # 2차 필터: 메인 피드인 경우에만 키워드 검사 진행 (타 매체는 전체 수집)
-                    if "theverge.com" or "autonews.com" or "greencarcongress.com" or "harvardbusinessreview" or "smartcitiesdive.com" in url:
-                        is_mobility_news = any(keyword in content_text for keyword in mobility_keywords)
+                    if "autonews.com" or "techcrunch.com" or "smartcitiesdive.com" or "theverge.com" or "harvardbusinessreview" or "technologyreview.com" in url:
+                        is_mobility_news = any(keyword in content_text for keyword in mobility_keywords or technology_keywords)
                     else:
                         is_mobility_news = True  # 다른 전문 매체는 조건 없이 통과
                     
